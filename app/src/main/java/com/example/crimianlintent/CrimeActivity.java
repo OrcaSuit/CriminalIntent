62390e640bf7c8d8f24030927b7f8bba18ac7aae
orcasuit <orcasuit@gmail.com>
2019-05-27 15:26:17 +0900

    CrimeActivity의 영역이 아닌 CrimeFragment에 속하는 곳에 Crime 객체 Id를 저장.

--- app/src/main/java/com/example/crimianlintent/CrimeActivity.java
+++ app/src/main/java/com/example/crimianlintent/CrimeActivity.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 public class CrimeActivity extends SingleFragmentActivity {
 
 
-    public static final String EXTRA_CRIME_ID="com.example.crimianlintent.crime_id";
+    private static final String EXTRA_CRIME_ID="com.example.crimianlintent.crime_id";
 
     public static Intent newIntent(Context packageContext, UUID crimeId) {
         Intent intent = new Intent(packageContext, CrimeActivity.class);
@@ -23,7 +23,8 @@ public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+        return  CrimeFragment.newInstance(crimeId);
     }
 }
 
