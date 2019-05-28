87c6b64dcac2e24436136aa5fb9d5c93cb38f23e
orcasuit <orcasuit@gmail.com>
2019-05-28 14:15:51 +0900

    AppCompat 라이브러리 이용, toolbar구현

--- app/src/main/java/com/example/crimianlintent/SingleFragmentActivity.java
+++ app/src/main/java/com/example/crimianlintent/SingleFragmentActivity.java
@@ -5,10 +5,11 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 import java.util.UUID;
 
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
 
