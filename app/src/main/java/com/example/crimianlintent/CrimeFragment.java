c1250438bc5d50af22669a0e70a90fa3132ce1e0
orcasuit <orcasuit@gmail.com>
2019-05-28 11:43:38 +0900

    DialogFragment생성

--- app/src/main/java/com/example/crimianlintent/CrimeFragment.java
+++ app/src/main/java/com/example/crimianlintent/CrimeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -25,6 +26,8 @@ import static java.time.OffsetDateTime.now;
 public class CrimeFragment extends Fragment {
 
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "DialogDate";
+
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
@@ -75,7 +78,15 @@ public class CrimeFragment extends Fragment {
 
         mDateButton =(Button)v.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.gettime());
-        mDateButton.setEnabled(false);
+        //mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener(){
+                                           @Override
+                                           public void onClick(View v) {
+                                           FragmentManager manager = getFragmentManager();
+                                           DatePickerFragment dialog = new DatePickerFragment();
+                                           dialog.show(manager, DIALOG_DATE);
+                                           }
+                                       });
 
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
---
+++ app/src/main/java/com/example/crimianlintent/DatePickerFragment.java
@@ -0,0 +1,16 @@
+package com.example.crimianlintent;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+
+public class DatePickerFragment extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity()).setTitle(R.string.date_picker_title).setPositiveButton(android.R.string.ok, null).create();
+    }
+}
