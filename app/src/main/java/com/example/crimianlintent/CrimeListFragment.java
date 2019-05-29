706068dd2e915a822e600d143c0724a75b64470c
orcasuit <orcasuit@gmail.com>
2019-05-29 11:47:13 +0900

    변경된 모델 데이터를 리스트에 반영

--- app/src/main/java/com/example/crimianlintent/CrimeListFragment.java
+++ app/src/main/java/com/example/crimianlintent/CrimeListFragment.java
@@ -117,6 +117,7 @@ public class CrimeListFragment extends Fragment {
             mAdapter = new CrimeAdapter(crimes);
             mCrimeRecyclerView.setAdapter(mAdapter);
         } else {
+            mAdapter.setCrimes(crimes);
             mAdapter.notifyDataSetChanged();
         }
         updateSubtitle();
@@ -173,6 +174,10 @@ public class CrimeListFragment extends Fragment {
         @Override
         public int getItemCount() {
             return mCrimes.size();
+        }
+
+        public void setCrimes(List<Crime> crimes){
+        mCrimes = crimes;
         }
 
         public CrimeAdapter(List<Crime> crimes) {
