0a8def03efa6fb15e0dea1439d52b03da5ed8e99
orcasuit <orcasuit@gmail.com>
2019-05-29 16:06:14 +0900

    으헿?

--- app/src/main/java/com/example/crimianlintent/SingleFragmentActivity.java
+++ app/src/main/java/com/example/crimianlintent/SingleFragmentActivity.java
@@ -3,11 +3,9 @@ package com.example.crimianlintent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
-import java.util.UUID;
 
 public abstract class SingleFragmentActivity extends AppCompatActivity {
 
