8bc138f34febc832e83109035aeab37f0f50780a
orcasuit <orcasuit@gmail.com>
2019-05-31 15:09:19 +0900

    레이아웃 유연성 추가

--- app/src/main/java/com/example/crimianlintent/SingleFragmentActivity.java
+++ app/src/main/java/com/example/crimianlintent/SingleFragmentActivity.java
@@ -1,6 +1,7 @@
 package com.example.crimianlintent;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -10,13 +11,18 @@ import android.support.v7.app.AppCompatActivity;
 import java.util.UUID;
 
 public abstract class SingleFragmentActivity extends AppCompatActivity {
-
     protected abstract Fragment createFragment();
+
+    @LayoutRes
+    protected int getLayoutResId(){
+        return R.layout.activity_crime_pager;
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
+        //setContentView(R.layout.activity_crime);
+        setContentView(getLayoutResId());
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
